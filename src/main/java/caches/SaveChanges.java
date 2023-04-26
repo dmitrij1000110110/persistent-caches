96426fc26d905ee7a1abed5e5d253c09eb0c72d8
dmitrij1000110110 <dimaibraev3008@gmail.com>
2023-04-26 16:15:51 +0300

    Update SaveChanges.java

--- src/main/java/caches/SaveChanges.java
+++ src/main/java/caches/SaveChanges.java
@@ -1,116 +1,119 @@
-package caches;
+package com.github.SuduIDE.persistentidecaches;
 
-import caches.changes.AddChange;
-import caches.changes.DeleteChange;
-import caches.changes.ModifyChange;
-import caches.changes.RenameChange;
-import caches.records.FilePointer;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-
-import java.io.File;
+import com.github.SuduIDE.persistentidecaches.changes.AddChange;
+import com.github.SuduIDE.persistentidecaches.changes.Change;
+import com.github.SuduIDE.persistentidecaches.changes.DeleteChange;
+import com.github.SuduIDE.persistentidecaches.changes.ModifyChange;
+import com.github.SuduIDE.persistentidecaches.changes.RenameChange;
+import com.github.SuduIDE.persistentidecaches.records.FilePointer;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 
-public class SaveChanges {
+public class VsCodeClient {
 
-    @Getter
-    @AllArgsConstructor
-    @NoArgsConstructor
-    private static class ModifyChangeFromJSON {
+    public static final String SEARCH = "search";
+    public static final String CHANGES = "changes";
 
-        long timestamp;
-        String uri;
-        String oldText;
-        String newText;
+    public static final String CHECKOUT = "checkout";
 
-    }
+    public static final String CC_SEARCH = "ccsearch";
+    public static final int BUSY_WAITING_MILLIS = 500;
+    private static final char[] BUFFER = new char[16384];
 
-    @Getter
-    @AllArgsConstructor
-    @NoArgsConstructor
-    private static class CreateFileChangeFromJSON {
+    @SuppressWarnings({"BusyWait", "InfiniteLoopStatement"})
+    public static void main(final String[] args) throws IOException, InterruptedException {
+        if (args.length < 1) {
+            throw new RuntimeException("Needs path to repository as first arg");
+        }
+        try (final IndexesManager manager = new IndexesManager(true)) {
+            final var trigramHistoryIndex = manager.addTrigramIndex();
+            final var trigramIndexUtils = trigramHistoryIndex.getTrigramIndexUtils();
+            final var repPath = Path.of(args[0]);
+            manager.parseGitRepository(repPath);
+            manager.getFileCache().forEach((it, iti) -> System.err.println(it + " " + iti));
 
-        long timestamp;
-        String uri;
-        String text;
-
-    }
-
-    @Getter
-    @AllArgsConstructor
-    @NoArgsConstructor
-    private static class DeleteFileChangeFromJSON {
-
-        long timestamp;
-        String uri;
-        String text;
-
-    }
-
-    @Getter
-    @AllArgsConstructor
-    @NoArgsConstructor
-    private static class RenameFileChangeFromJSON {
-
-        long timestamp;
-        String oldUri;
-        String newUri;
-        String text;
-
-    }
-
-    @Getter
-    @AllArgsConstructor
-    @NoArgsConstructor
-    private static class Changes {
-
-        List<ModifyChangeFromJSON> modifyChanges;
-        List<CreateFileChangeFromJSON> createChanges;
-        List<DeleteFileChangeFromJSON> deleteChanges;
-        List<RenameFileChangeFromJSON> renameChanges;
-
-    }
-
-    public static void main(String[] args) throws JsonProcessingException {
-        System.out.println(args[0]);
-        ObjectMapper objectMapper = new ObjectMapper();
-        Scanner scanner = new Scanner(System.in);
-        while (scanner.hasNextLine()) {
-            String line = scanner.nextLine();
-            if (line.equals("changes")) {
-                line = scanner.nextLine();
-                Changes changes = objectMapper.readValue(line, Changes.class);
-                for (ModifyChangeFromJSON modifyChangeFromJSON : changes.modifyChanges) {
-                    ModifyChange modifyChange = new ModifyChange(modifyChangeFromJSON.timestamp,
-                            () -> modifyChangeFromJSON.oldText,
-                            () -> modifyChangeFromJSON.newText,
-                            new File(modifyChangeFromJSON.uri),
-                            new File(modifyChangeFromJSON.uri));
+            final ObjectMapper objectMapper = new ObjectMapper();
+            final BufferedReader scanner = new BufferedReader(new InputStreamReader(System.in));
+            while (true) {
+                String line = scanner.readLine();
+                Thread.sleep(BUSY_WAITING_MILLIS);
+                if (line.equals(CHANGES)) {
+                    final var read = scanner.read(BUFFER);
+                    line = new String(BUFFER, 0, read);
+                    final Changes changes = objectMapper.readValue(line, Changes.class);
+                    final List<Change> processedChangesList = new ArrayList<>();
+                    for (final ModifyChangeFromJSON modifyChangeFromJSON : changes.modifyChanges) {
+                        final Path path = repPath.relativize(Path.of(modifyChangeFromJSON.uri));
+                        final ModifyChange modifyChange = new ModifyChange(changes.timestamp,
+                                () -> modifyChangeFromJSON.oldText,
+                                () -> modifyChangeFromJSON.newText,
+                                path,
+                                path);
+                        processedChangesList.add(modifyChange);
+                    }
+                    for (final CreateFileChangeFromJSON createFileChangeFromJSON : changes.addChanges) {
+                        final AddChange addChange = new AddChange(changes.timestamp,
+                                new FilePointer(repPath.relativize(Path.of(createFileChangeFromJSON.uri)), 0),
+                                createFileChangeFromJSON.text);
+                        processedChangesList.add(addChange);
+                    }
+                    for (final DeleteFileChangeFromJSON deleteFileChangeFromJSON : changes.deleteChanges) {
+                        final DeleteChange deleteChange = new DeleteChange(changes.timestamp,
+                                new FilePointer(repPath.relativize(Path.of(deleteFileChangeFromJSON.uri)), 0),
+                                deleteFileChangeFromJSON.text);
+                        processedChangesList.add(deleteChange);
+                    }
+                    for (final RenameFileChangeFromJSON renameFileChangeFromJSON : changes.renameChanges) {
+                        final RenameChange renameChange = new RenameChange(changes.timestamp,
+                                () -> renameFileChangeFromJSON.text,
+                                () -> renameFileChangeFromJSON.text,
+                                repPath.relativize(Path.of(renameFileChangeFromJSON.oldUri)),
+                                repPath.relativize(Path.of(renameFileChangeFromJSON.newUri)));
+                        processedChangesList.add(renameChange);
+                    }
+                    manager.nextRevision();
+                    manager.applyChanges(processedChangesList);
+                } else if (line.equals(SEARCH)) {
+                    final var read = scanner.read(BUFFER);
+                    line = new String(BUFFER, 0, read);
+                    System.out.println(trigramIndexUtils.filesForString(line));
+                } else if (line.equals(CHECKOUT)) {
+                    final var read = scanner.read(BUFFER);
+                    line = new String(BUFFER, 0, read);
+                } else if (line.equals(CC_SEARCH)) {
+                    final var read = scanner.read(BUFFER);
+                    line = new String(BUFFER, 0, read);
                 }
-                for (CreateFileChangeFromJSON createFileChangeFromJSON : changes.createChanges) {
-                    AddChange addChange = new AddChange(createFileChangeFromJSON.timestamp,
-                            new FilePointer(new File(createFileChangeFromJSON.uri), 0),
-                            createFileChangeFromJSON.text);
-                }
-                for (DeleteFileChangeFromJSON deleteFileChangeFromJSON : changes.deleteChanges) {
-                    DeleteChange deleteChange = new DeleteChange(deleteFileChangeFromJSON.timestamp,
-                            new FilePointer(new File(deleteFileChangeFromJSON.uri), 0),
-                            deleteFileChangeFromJSON.text);
-                }
-                for (RenameFileChangeFromJSON renameFileChangeFromJSON : changes.renameChanges) {
-                    RenameChange renameChange = new RenameChange(renameFileChangeFromJSON.timestamp,
-                            () -> renameFileChangeFromJSON.text,
-                            () -> renameFileChangeFromJSON.text,
-                            new File(renameFileChangeFromJSON.oldUri),
-                            new File(renameFileChangeFromJSON.newUri));
-                }
-            } else if (line.equals("search")) {
-                line = scanner.nextLine();
             }
         }
+    }
+
+    private record ModifyChangeFromJSON(String uri, String oldText, String newText) {
+
+    }
+
+    private record CreateFileChangeFromJSON(String uri, String text) {
+
+    }
+
+    private record DeleteFileChangeFromJSON(String uri, String text) {
+
+    }
+
+    private record RenameFileChangeFromJSON(String oldUri, String newUri, String text) {
+
+    }
+
+    private record Changes(List<ModifyChangeFromJSON> modifyChanges,
+                           List<CreateFileChangeFromJSON> addChanges,
+                           List<DeleteFileChangeFromJSON> deleteChanges,
+                           List<RenameFileChangeFromJSON> renameChanges,
+                           long timestamp) {
+
     }
 }
